64ed54aa0071429af81ff1ab5ebe84a10e74aefe
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-11-21 21:14:26 +0000

    feat: add tests for final solution to the template

--- template/YYYY_DD.py
+++ template/YYYY_DD.py
@@ -10,8 +10,14 @@ def solution_YYYY_DD_B(filename: str) -> int:
 def test_solution_YYYY_DD_A():
     assert solution_YYYY_DD_A('./YYYY_DD/test_input.txt') == 0 # Replace with expected output for the test case
 
+#def test_final_solution_YYYY_DD_A():
+#    assert solution_YYYY_DD_A('./YYYY_DD/input.txt') == 0 # Replace with solution when known
+
 def test_solution_YYYY_DD_B():
     assert solution_YYYY_DD_B('./YYYY_DD/test_input.txt') == 0 # Replace with expected output for the test case
+
+#def test_final_solution_YYYY_DD_B():
+#    assert solution_YYYY_DD_B('./YYYY_DD/input.txt') == 0 # Replace with solution when known
 
 if __name__ == '__main__':
     run('YYYY_DD_A', solution_YYYY_DD_A)
