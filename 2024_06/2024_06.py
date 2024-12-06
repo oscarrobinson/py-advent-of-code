f11a61b6a21a925131381061a6225fac86195be3
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-12-06 07:36:19 +0000

    feat: 2024 6 b

--- 2024_06/2024_06.py
+++ 2024_06/2024_06.py
@@ -63,19 +63,65 @@ def solution_2024_06_A(filename: str) -> int:
 
 
 def solution_2024_06_B(filename: str) -> int:
-    return 0
+    # for each position, add an obstacle to map
+
+    # create a set of tuples (guard_x, guard_y, guard_vel_x, guard_vel_y)
+    # iterate over guard positions as before, if encounter a guard_pos already in the set we have a loop
+    #       therefore increment result
+
+    # TODO: This is slow cos we consider every single point to put an obstacle on
+    #       Instead could just consider points the guard visits on their normal path
+    with open(filename) as lines:
+        result = 0
+        lines = list(lines)
+        guard_map = grid_from_lines(lines)
+        poss_positions = guard_map.width() * guard_map.height()
+        pos_considered = 0
+        print(guard_map)
+        for x in range(0, guard_map.width()):
+            for y in range(0, guard_map.height()):
+                pos_considered += 1
+                print(f"{pos_considered}/{poss_positions}")
+
+                guard_map = grid_from_lines(lines)
+                guard = get_guard_pos(guard_map)
+
+                if guard_map.val(Point(x, y)) != "#" and guard.point != Point(x, y):
+                    guard_map.set(Point(x, y), "#")
+                    guard_map.set(guard.point, ".")
+
+                    vel_x, vel_y = get_guard_vel(guard)
+                    pos_visited_at_vel = set()
+
+                    while guard_on_map(guard, guard_map):
+                        next_point = Point(guard.point.x + vel_x, guard.point.y + vel_y)
+                        if guard_map.val(next_point) == "#":
+                            guard = rotate_guard(guard)
+                            vel_x, vel_y = get_guard_vel(guard)
+                        else:
+                            guard = Cell(next_point, guard.val)
+                            pos_and_vel = (guard.point.x, guard.point.y, vel_x, vel_y)
+
+                            loop_detected = pos_and_vel in pos_visited_at_vel
+
+                            if loop_detected:
+                                result += 1
+                                break
+                            pos_visited_at_vel.add(pos_and_vel)
+
+        return result
 
 
 def test_solution_2024_06_A():
     assert solution_2024_06_A("./2024_06/test_input.txt") == 41  # Replace with expected output for the test case
 
 
-# def test_final_solution_2024_06_A():
-#    assert solution_2024_06_A('./2024_06/input.txt') == 0 # Replace with solution when known
+def test_final_solution_2024_06_A():
+    assert solution_2024_06_A("./2024_06/input.txt") == 4964  # Replace with solution when known
 
 
 def test_solution_2024_06_B():
-    assert solution_2024_06_B("./2024_06/test_input.txt") == 0  # Replace with expected output for the test case
+    assert solution_2024_06_B("./2024_06/test_input.txt") == 6  # Replace with expected output for the test case
 
 
 # def test_final_solution_2024_06_B():
