e816adddf1338cda505043a60121409909667219
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-12-06 19:11:46 +0000

    2023-6-B: refactor and runtime improvements

--- 2024_06/2024_06.py
+++ 2024_06/2024_06.py
@@ -42,74 +42,71 @@ def guard_on_map(guard: Cell, guard_map: Grid) -> bool:
     return g_x < width and g_x >= 0 and g_y < height and g_y >= 0
 
 
+def get_points_visited(guard: Cell, guard_map: Grid) -> set[Point]:
+    vel_x, vel_y = get_guard_vel(guard)
+    pos_visited = set()
+
+    while guard_on_map(guard, guard_map):
+        pos_visited.add((guard.point.x, guard.point.y))
+        next_point = Point(guard.point.x + vel_x, guard.point.y + vel_y)
+        if guard_map.val(next_point) == "#":
+            guard = rotate_guard(guard)
+            vel_x, vel_y = get_guard_vel(guard)
+        else:
+            guard = Cell(next_point, guard.val)
+    return pos_visited
+
+
 def solution_2024_06_A(filename: str) -> int:
     with open(filename) as lines:
         guard_map = grid_from_lines(list(lines))
-        guard = get_guard_pos(guard_map)
-        guard_map.set(guard.point, ".")
-        vel_x, vel_y = get_guard_vel(guard)
-        pos_visited = set()
-
-        while guard_on_map(guard, guard_map):
-            next_point = Point(guard.point.x + vel_x, guard.point.y + vel_y)
-            if guard_map.val(next_point) == "#":
-                guard = rotate_guard(guard)
-                vel_x, vel_y = get_guard_vel(guard)
-            else:
-                guard = Cell(next_point, guard.val)
-                pos_visited.add((guard.point.x, guard.point.y))
-
-    return len(pos_visited) - 1
+    guard = get_guard_pos(guard_map)
+    return len(get_points_visited(guard, guard_map))
 
 
 def solution_2024_06_B(filename: str) -> int:
-    # for each position, add an obstacle to map
-
-    # create a set of tuples (guard_x, guard_y, guard_vel_x, guard_vel_y)
-    # iterate over guard positions as before, if encounter a guard_pos already in the set we have a loop
-    #       therefore increment result
-
-    # TODO: This is slow cos we consider every single point to put an obstacle on
-    #       Instead could just consider points the guard visits on their normal path
     with open(filename) as lines:
-        result = 0
         lines = list(lines)
+    guard_map = grid_from_lines(lines)
+    guard = get_guard_pos(guard_map)
+    points_visited = get_points_visited(guard, guard_map)
+    poss_positions = len(points_visited)
+    pos_considered = 0
+    result = 0
+    # We'll try and put an obstacle at every point in the guard's path
+    # We then check if playing the guard's route results in a cycle
+    for x, y in points_visited:
+        point = Point(x, y)
+        pos_considered += 1
+        print(f"{pos_considered}/{poss_positions}")
+
         guard_map = grid_from_lines(lines)
-        poss_positions = guard_map.width() * guard_map.height()
-        pos_considered = 0
-        print(guard_map)
-        for x in range(0, guard_map.width()):
-            for y in range(0, guard_map.height()):
-                pos_considered += 1
-                print(f"{pos_considered}/{poss_positions}")
+        guard = get_guard_pos(guard_map)
 
-                guard_map = grid_from_lines(lines)
-                guard = get_guard_pos(guard_map)
+        if guard_map.val(point) != "#" and guard.point != point:
+            guard_map.set(point, "#")
+            guard_map.set(guard.point, ".")
 
-                if guard_map.val(Point(x, y)) != "#" and guard.point != Point(x, y):
-                    guard_map.set(Point(x, y), "#")
-                    guard_map.set(guard.point, ".")
+            vel_x, vel_y = get_guard_vel(guard)
+            pos_visited_at_vel = set()
 
+            while guard_on_map(guard, guard_map):
+                next_point = Point(guard.point.x + vel_x, guard.point.y + vel_y)
+                if guard_map.val(next_point) == "#":
+                    guard = rotate_guard(guard)
                     vel_x, vel_y = get_guard_vel(guard)
-                    pos_visited_at_vel = set()
+                else:
+                    guard = Cell(next_point, guard.val)
+                    pos_and_vel = (guard.point.x, guard.point.y, vel_x, vel_y)
 
-                    while guard_on_map(guard, guard_map):
-                        next_point = Point(guard.point.x + vel_x, guard.point.y + vel_y)
-                        if guard_map.val(next_point) == "#":
-                            guard = rotate_guard(guard)
-                            vel_x, vel_y = get_guard_vel(guard)
-                        else:
-                            guard = Cell(next_point, guard.val)
-                            pos_and_vel = (guard.point.x, guard.point.y, vel_x, vel_y)
+                    loop_detected = pos_and_vel in pos_visited_at_vel
 
-                            loop_detected = pos_and_vel in pos_visited_at_vel
+                    if loop_detected:
+                        result += 1
+                        break
+                    pos_visited_at_vel.add(pos_and_vel)
 
-                            if loop_detected:
-                                result += 1
-                                break
-                            pos_visited_at_vel.add(pos_and_vel)
-
-        return result
+    return result
 
 
 def test_solution_2024_06_A():
