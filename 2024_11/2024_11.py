f5232c1786085347d42b4abe0042a1de1ed31bde
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-12-11 07:03:17 +0000

    2024-11-B: Some notes, brute force is too slow

--- 2024_11/2024_11.py
+++ 2024_11/2024_11.py
@@ -1,5 +1,19 @@
 import sys
 from common.utils import run
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1000000)
+def get_next_numbers(num_str: str) -> list[str]:
+    new_nums = []
+    if int(num_str) == 0:
+        new_nums.append("1")
+    elif len(num_str) % 2 == 0:
+        new_nums.append(str(int(num_str[0 : int(len(num_str) / 2)])))
+        new_nums.append(str(int(num_str[int(len(num_str) / 2) :])))
+    else:
+        new_nums.append(str(int(num_str) * 2024))
+    return new_nums
 
 
 def get_num_stones(filename: str, after_blinks: int) -> int:
@@ -7,16 +21,18 @@ def get_num_stones(filename: str, after_blinks: int) -> int:
         line = list(lines)[0].strip().split()
 
     for i in range(0, after_blinks):
+        print("\n--------\n")
+        print(get_next_numbers.cache_info())
         print(i)
         new_line = []
+        j = 0
+        length = len(line)
+        print(length)
         for num_str in line:
-            if int(num_str) == 0:
-                new_line.append("1")
-            elif len(num_str) % 2 == 0:
-                new_line.append(str(int(num_str[0 : int(len(num_str) / 2)])))
-                new_line.append(str(int(num_str[int(len(num_str) / 2) :])))
-            else:
-                new_line.append(str(int(num_str) * 2024))
+            j += 1
+            print(str(j) + "\r", end="")
+            for num in get_next_numbers(num_str):
+                new_line.append(num)
         line = new_line
 
     return len(line)
@@ -26,9 +42,12 @@ def solution_2024_11_A(filename: str) -> int:
     return get_num_stones(filename, 25)
 
 
+# TODO: Cache grows very slowly, this means same numbers come up again and again
+# each application of the rules must form a cycle
+
+
 def solution_2024_11_B(filename: str) -> int:
-    return 0
-    # return get_num_stones(filename, 75)
+    return get_num_stones(filename, 75)
 
 
 def test_solution_2024_11_A():
