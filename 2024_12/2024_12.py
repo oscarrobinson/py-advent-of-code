acb21eec89aea6070a731553ad27b045804f2e22
Oscar Robinson <oscar.robinson@beamery.com>
2024-12-12 13:38:41 +0000

    2024-12-B: Progress, not done yet

--- 2024_12/2024_12.py
+++ 2024_12/2024_12.py
@@ -1,6 +1,6 @@
 import sys
 from common.utils import run
-from common.grid import grid_from_lines
+from common.grid import grid_from_lines, Point
 
 
 def solution_2024_12_A(filename: str) -> int:
@@ -35,6 +35,159 @@ def solution_2024_12_A(filename: str) -> int:
 
 
 def solution_2024_12_B(filename: str) -> int:
+    with open(filename) as file:
+        lines = list(file)
+    farm = grid_from_lines(lines)
+    visited = set()
+    total_fence_cost = 0
+
+    regions = []
+
+    for plot in farm:
+        plant = plot.val
+        if plot.point not in visited:
+            stack = [plot.point]
+            region = set()
+            while stack:
+                cur_plot = stack.pop()
+                if cur_plot not in region:
+                    region.add(cur_plot)
+                    visited.add(cur_plot)
+                    for neighbour in farm.get_neighbours(cur_plot):
+                        if neighbour.val == plant and neighbour.point not in visited:
+                            stack.append(neighbour.point)
+            regions.append(region)
+    
+    for region in regions:
+        # Sort by x coord then y coord order so are walking points by column in order
+        column_order = sorted(region, key=lambda p: (p.x, p.y))
+        prev_point = None
+        prev_prev_point = None
+        on_side = False
+        sides = 0
+        # Check the left side of every point
+        for point in column_order:
+            # If already on a side, check if we're still on the same side
+            if on_side:
+                # no longer on same side if point to direct left is in same region we haven't taken a contiguous step down
+                if farm.val(point) == farm.val(Point(point.x-1, point.y)) or point.y != prev_point.y+1:
+                    on_side = False
+                    sides += 1
+
+            # If not already on a side/no longer on a side, check if we're on a new side
+            if not on_side:
+                # on a side if point to the direct left is out of bounds or a different plant
+                if farm.val(point) != farm.val(Point(point.x-1, point.y)):
+                    on_side = True
+
+            prev_prev_point = prev_point
+            prev_point = point
+        
+        # Check if our final point counts as a side
+        if on_side:
+            # no longer on same side if point to direct right is in same region we haven't taken a contiguous step down
+            if farm.val(prev_point) == farm.val(Point(prev_point.x-1, prev_point.y)) or not prev_prev_point or prev_point.y != prev_prev_point.y+1:
+                on_side = False
+                sides += 1
+
+        prev_point = None
+        prev_prev_point = None
+        on_side = False
+
+        # Check the right side of every point
+        for point in column_order:
+            # If already on a side, check if we're still on the same side
+            if on_side:
+                # no longer on same side if point to direct right is in same region we haven't taken a contiguous step down
+                if farm.val(point) == farm.val(Point(point.x+1, point.y)) or point.y != prev_point.y+1:
+                    on_side = False
+                    sides += 1
+
+            # If not already on a side/no longer on a side, check if we're on a new side
+            if not on_side:
+                # on a side if point to the direct right is out of bounds or a different plant
+                if farm.val(point) != farm.val(Point(point.x+1, point.y)):
+                    on_side = True
+
+            prev_prev_point = prev_point
+            prev_point = point
+
+        # Check if our final point counts as a side
+        if on_side:
+            # no longer on same side if point to direct right is in same region we haven't taken a contiguous step down
+            if farm.val(prev_point) == farm.val(Point(prev_point.x+1, prev_point.y)) or not prev_prev_point or prev_point.y != prev_prev_point.y+1:
+                on_side = False
+                sides += 1
+
+
+        prev_point = None
+        prev_prev_point = None
+        on_side = False
+
+        # Sort by y coord then x coord order so are walking points by row in order
+        row_order = sorted(region, key=lambda p: (p.y, p.x))
+        prev_point = None
+        prev_prev_point = None
+        on_side = False
+        # Check the top side of every point
+        for point in row_order:
+            print(point)
+
+            # If already on a side, check if we're still on the same side
+            if on_side:
+                # no longer on same side if point to direct above is in same region we haven't taken a contiguous step right
+                if farm.val(point) == farm.val(Point(point.x, point.y-1)) or point.x != prev_point.x+1:
+                    on_side = False
+                    sides += 1
+
+            # If not already on a side/no longer on a side, check if we're on a new side
+            if not on_side:
+                # on a side if point to the direct above is out of bounds or a different plant
+                if farm.val(point) != farm.val(Point(point.x, point.y-1)):
+                    on_side = True
+
+            prev_prev_point = prev_point
+            prev_point = point
+        
+        # Check if our final point counts as a side
+        if on_side:
+            # no longer on same side if point direct above is in same region we haven't taken a contiguous step right
+            if farm.val(prev_point) == farm.val(Point(prev_point.x, prev_point.y-1)) or not prev_prev_point or prev_point.x != prev_prev_point.x+1:
+                on_side = False
+                sides += 1
+
+        prev_point = None
+        prev_prev_point = None
+        on_side = False
+
+        # Check the below side of every point
+        for point in row_order:
+            # If already on a side, check if we're still on the same side
+            if on_side:
+                # no longer on same side if point to direct right is in same region we haven't taken a contiguous step right
+                if farm.val(point) == farm.val(Point(point.x, point.y+1)) or point.x != prev_point.x+1:
+                    on_side = False
+                    sides += 1
+
+            # If not already on a side/no longer on a side, check if we're on a new side
+            if not on_side:
+                # on a side if point direct below is out of bounds or a different plant
+                if farm.val(point) != farm.val(Point(point.x, point.y+1)):
+                    on_side = True
+
+            prev_prev_point = prev_point
+            prev_point = point
+
+        # Check if our final point counts as a side
+        if on_side:
+            # no longer on same side if point to direct below is in same region we haven't taken a contiguous step down
+            if farm.val(prev_point) == farm.val(Point(prev_point.x, prev_point.y+1)) or not prev_prev_point or prev_point.x != prev_prev_point.x+1:
+                on_side = False
+                sides += 1
+
+        print("\n")
+        print(sides)
+        print("--------")
     return 0
 
 
@@ -47,7 +200,7 @@ def test_solution_2024_12_A():
 
 
 def test_solution_2024_12_B():
-    assert solution_2024_12_B("./2024_12/test_input.txt") == 0  # Replace with expected output for the test case
+    assert solution_2024_12_B("./2024_12/test_input.txt") == 1206  # Replace with expected output for the test case
 
 
 # def test_final_solution_2024_12_B():
