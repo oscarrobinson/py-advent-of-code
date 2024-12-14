724c09ff23d28f787e4060149c10fe648d42b651
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-12-14 07:47:07 +0000

    2024-14-B: Done

--- 2024_14/2024_14.py
+++ 2024_14/2024_14.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import math
+import time
 from common.utils import run
 
 
@@ -48,6 +49,69 @@ def solution_2024_14_A(filename: str) -> int:
 
 
 def solution_2024_14_B(filename: str) -> int:
+    room_width = 101
+    room_height = 103
+    iterations = 1
+
+    rgx = re.compile("p=([\\-0-9]+),([\\-0-9]+) v=([\\-0-9]+),([\\-0-9]+)")
+    robots = []
+
+    with open(filename) as lines:
+        for line in lines:
+            p_x, p_y, v_x, v_y = rgx.match(line).group(1, 2, 3, 4)
+            robots.append([int(p_x), int(p_y), int(v_x), int(v_y)])
+
+    iteration = 0
+
+    # We'll print out anything xmas tree-y and inspect manually to determine if it's a tree
+    while True:
+        should_print = False
+
+        # Check if we have a row with more than 10 adjacent robots as that may indicate base of a xmas tree
+        robot_tuples = sorted([(robot[0], robot[1]) for robot in robots])
+
+        prev_x = -1
+        prev_y = -1
+        adjacent = 0
+
+        for robot in robot_tuples:
+            x = robot[0]
+            y = robot[1]
+
+            if x == prev_x and y == prev_y + 1:
+                adjacent += 1
+            else:
+                adjacent = 0
+
+            if adjacent >= 10:
+                should_print = True
+
+            prev_x = x
+            prev_y = y
+
+        # We're only printing if we think there might be an xmas tree
+        if should_print:
+            print(iteration)
+
+            for y in range(0, room_height):
+                for x in range(0, room_width):
+                    if any([robot[0] == x and robot[1] == y for robot in robots]):
+                        print("x", end="")
+                    else:
+                        print(".", end="")
+                print("\n")
+
+            print("------------------------------")
+
+            time.sleep(0.5)
+
+        # Iterate the robots
+        for i in range(0, len(robots)):
+            robots[i][0] = (robots[i][0] + (robots[i][2] * iterations)) % room_width
+            robots[i][1] = (robots[i][1] + (robots[i][3] * iterations)) % room_height
+
+        iteration += 1
+
     return 0
 
 
@@ -59,8 +123,8 @@ def test_final_solution_2024_14_A():
     assert solution_2024_14_A("./2024_14/input.txt") == 218619324  # Replace with solution when known
 
 
-def test_solution_2024_14_B():
-    assert solution_2024_14_B("./2024_14/test_input.txt") == 0  # Replace with expected output for the test case
+# def test_solution_2024_14_B():
+#    assert solution_2024_14_B("./2024_14/test_input.txt") == 0  # Replace with expected output for the test case
 
 
 # def test_final_solution_2024_14_B():
