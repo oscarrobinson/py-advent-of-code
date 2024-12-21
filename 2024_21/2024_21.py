fb41a484b6b452ea77ce834a99c7174a19d64a0a
Oscar Robinson <oscar.w.robinson@gmail.com>
2024-12-21 21:55:46 +0000

    2024-21-A: Done

--- 2024_21/2024_21.py
+++ 2024_21/2024_21.py
@@ -80,8 +80,33 @@ def get_shortest_press_seqs(cur_button: str, rem_seq: str, buttons: Grid) -> lis
                 poss_subsequent_paths = get_shortest_press_seqs(rem_seq[0:1], rem_seq[1:], buttons)
                 for sub_path in poss_subsequent_paths:
                     result.append("".join(header) + sub_path)
-        print(result)
         return result
+
+
+#     +---+---+
+#     | ^ | A |
+# +---+---+---+
+# | < | v | > |
+# +---+---+---+
+
+press_dist_mapping = {
+    "<": {"v": 1, "^": 2, ">": 2, "A": 3, "<": 0},
+    "v": {"<": 1, ">": 1, "^": 1, "A": 2, "v": 0},
+    ">": {"v": 1, "A": 1, "^": 2, "<": 2, ">": 0},
+    "^": {"A": 1, "v": 1, ">": 2, "<": 2, "^": 0},
+    "A": {"^": 1, ">": 1, "v": 2, "<": 3, "A": 0},
+}
+
+
+def directional_press_seq_to_length(press_seq: str) -> int:
+    total = 0
+    for i in range(0, len(press_seq) - 2):
+        cur_button = press_seq[i]
+        next_button = press_seq[i + 1]
+        dist = press_dist_mapping[cur_button][next_button]
+        total += dist
+        total += 1  # +1 cos we need to press A to then make the robot press the button
+    return total
 
 
 def shortest_press_seq_length(numeric_seq: str) -> int:
@@ -92,25 +117,13 @@ def shortest_press_seq_length(numeric_seq: str) -> int:
     robot_3_press_seqs = [
         final_seq for seq in robot_2_press_seqs for final_seq in get_shortest_press_seqs("A", seq, directional_grid)
     ]
-    my_press_seq_lens = [
-        len(final_seq)
-        for seq in robot_3_press_seqs
-        for final_seq in get_shortest_press_seqs("A", seq, directional_grid)
-    ]
-    return min(my_press_seq_lens)
-
-
-# TODO:
-# current approach uses too much memory
-# Could Pre-compute shortest path on my keypad to move from every point
-# on numeric keypad to every other point on numeric keypad
-# then will be quick to get each result
+    return min([len(seq) for seq in robot_3_press_seqs])
 
 
 def solution_2024_21_A(filename: str) -> int:
     result = 0
     with open(filename) as lines:
-        for line in lines:
+        for line in list(lines):
             length = shortest_press_seq_length(line.strip())
             # all input lines are three digits followed by A so just be lazy here...
             num_part = int(line[0:3])
